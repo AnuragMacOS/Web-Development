ab447022d39b213de18bb8663839b4307ab913d0
Anurag5 <anurag_23it183@dtu.ac.in>
2026-08-22 23:48:12 +0545

    Add files via upload

---
+++ React/01-basic-vite/src/App.jsx
@@ -0,0 +1,14 @@
+import Lorem from './Lorem'
+function App() {
+  const username = "mainuddinakash"
+  return (
+    <>
+      <h1>Hello World</h1>
+      {username}
+      <p>This is a paragraph</p>
+      <Lorem />
+    </>
+  )
+}
+
+export default App
